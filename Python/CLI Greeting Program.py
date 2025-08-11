4b4b9740760fb33ed122958cb250146e85cbdf56
mohameed-idris-17 <idrisvolley17@gmail.com>
2025-08-11 11:58:11 +0530

    Greeting program

---
+++ Python/CLI Greeting Program.py
@@ -0,0 +1,6 @@
+name = str(input("Enter your name:"))
+ID = int(input("Enter your ID number:"))
+def greeting(name,ID):
+    print(f"Hey {name},welcome to peri university")
+    print(f"Your class room number is BS6 allocated for your ID number {ID}")
+greeting(name,ID)
